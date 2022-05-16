2e73c487722f7dc43590d7409029d36e1da66bb5
unknown <luispomare92@gmail.com>
2022-05-16 15:27:53 -0500

    Create footer and card skelleton

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import Icon from './image/logo.jpg';
 
 const imge = document.querySelector('#logoimg');
 imge.src = Icon;
-// const Test= document.querySelector('.test');
-// const myIcon = new Image();
-// myIcon.src = Icon;
-// Test.appendChild(myIcon);
-//   console.log(Test.nodeName);
+const image2 = document.querySelector('.image');
+image2.src = Icon;
