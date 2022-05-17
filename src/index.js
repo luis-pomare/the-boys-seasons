dfda59225b379c61cf5b60faff681b2b54e56a46
unknown <luispomare92@gmail.com>
2022-05-17 07:52:41 -0500

    Fix header size and logo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 import storage from './modules/storage';
+import Icon from './image/logo.jpg';
+
+document.getElementById('logoimg').src = Icon; // Main logo displaying
 
 storage.getSeasonOne();
 storage.getSeasonTwo();
