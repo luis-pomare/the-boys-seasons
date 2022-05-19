911760de5b4d7e6e02b57fafdf66d510c1626594
Kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-05-19 23:11:59 +0530

    Update counter.test.js

--- src/modules/counter.test.js
+++ src/modules/counter.test.js
@@ -1,8 +1,7 @@
 /** * @jest-environment jsdom */
 import commentCount from './commentCount.js';
 
-describe ('Counter comments test',() => {
-
+describe('Counter comments test', () => {
   test('Return 1 if one item', () => {
     const data = [{ creation_date: '2022-05-18', username: 'Barbra', comment: 'Wow ! i was on toes when Milk finally learned about the Supes.❤️😁' }];
     expect(commentCount(data)).toBe(1);
