e8a0aee047784b19307f4573fa8a6af7469cff11
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-05-19 22:36:47 +0530

    Counter comment Test

---
+++ src/modules/counter.test.js
@@ -0,0 +1,10 @@
+/** * @jest-environment jsdom */
+import commentCount from './commentCount.js';
+
+describe ('Counter comments test',() => {
+
+  test('Return 1 if one item', () => {
+    const data = [{ creation_date: '2022-05-18', username: 'Barbra', comment: 'Wow ! i was on toes when Milk finally learned about the Supes.❤️😁' }];
+    expect(commentCount(data)).toBe(1);
+  });
+});
